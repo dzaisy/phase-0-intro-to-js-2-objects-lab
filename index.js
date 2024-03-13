0f995e20c90420a0fe9cc4e07c6ec6d3fbd42941
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-13 23:26:02 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    employeename: "zoe",
+    streetAddress: '225 nowhere lane',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newEmployee = Object.assign({}, employee, {[key]:value} );
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
